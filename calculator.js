c14fb9a34cc4fc8f45308188d7ccfd95f7085389
James Couball <jcouball@yahoo.com>
2025-08-18 16:50:00 -0700

    Create initial calculator methods add/subtract/multiply/divide

---
+++ calculator.js
@@ -0,0 +1,15 @@
+function add(operand1, operand2) {
+  return Number(operand1) + Number(operand2);
+}
+
+function subtract(operand1, operand2) {
+  return Number(operand1) - Number(operand2);
+}
+
+function multiply(operand1, operand2) {
+  return Number(operand1) * Number(operand2);
+}
+
+function divide(operand1, operand2) {
+  return Number(operand1) / Number(operand2);
+}
