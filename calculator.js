4ce713be84a6f2d31aa0b8560dee1a2dc352df81
James Couball <jcouball@yahoo.com>
2025-08-18 16:54:27 -0700

    Create the operate method to dispatch the operation to the right method

--- calculator.js
+++ calculator.js
@@ -13,3 +13,16 @@ function multiply(operand1, operand2) {
 function divide(operand1, operand2) {
   return Number(operand1) / Number(operand2);
 }
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case '+':
+      return add(num1, num2);
+    case '-':
+      return subtract(num1, num2);
+    case '*':
+      return multiply(num1, num2);
+    case '/':
+      return divide(num1, num2);
+  }
+}
